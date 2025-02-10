3b80c521c491d657c0d944c436cd24a244f75afd
Ilia <32134793+IliaBystrov@users.noreply.github.com>
2025-02-10 18:28:55 +0300

    task1

---
+++ Task1/task1.py
@@ -0,0 +1,20 @@
+n = int(input())
+m = int(input())
+a = []
+
+for i in range(n):
+    a.append(i+1)
+
+last = 0
+j = 0
+
+while a[0] != last:
+    f = 1
+    print(a[j], end=" ")
+    while f < m:
+        if j == len(a)-1:
+            j=0
+        else:
+            j+=1
+        f+=1
+    last = a[j]
