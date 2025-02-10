62754fce561f642b1742416842e5f6d14a4eb8d5
Ilia <32134793+IliaBystrov@users.noreply.github.com>
2025-02-10 19:02:47 +0300

    task2

---
+++ Task2/task2.py
@@ -0,0 +1,21 @@
+points = []
+circle = ''
+
+with open('points.txt', 'r') as f_points:
+    for line in f_points:
+        x, y = line.split(' ')
+        points.append({'x': float(x), 'y': float(y)})
+
+with open('circle.txt', 'r') as f_circle:
+    lines = f_circle.readlines()
+    x, y = lines[0].split(' ')
+    circle = {'x': float(x), 'y': float(y), 'r': float(lines[1])}
+
+for point in points:
+    s = ((point['x'] - circle['x'])**2) + ((point['y'] - circle['y'])**2)
+    if s == circle['r']**2:
+        print(0)
+    elif s < circle['r']**2:
+        print(1)
+    elif s > circle['r']**2:
+        print(2)
